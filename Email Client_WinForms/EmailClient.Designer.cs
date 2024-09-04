53e0ae69c5890b0c978e52e8936306384193a1ad
Pierre Joubert <joubertpierre96@gmail.com>
2024-09-04 14:00:54 +0200

    Repository initial creation
    First initial commit. All project files included.

---
+++ Email Client_WinForms/EmailClient.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Email_Client_WinForms
+{
+    partial class EmailClient
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmailClient));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(89, 45);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(90, 90);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Green;
+            label1.Location = new Point(74, 138);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 22);
+            label1.TabIndex = 1;
+            label1.Text = "Send Email";
+            // 
+            // EmailClient
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(259, 461);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Arial", 9F);
+            ForeColor = Color.Black;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "EmailClient";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Email Application";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
---
+++ Email Client_WinForms/EmailClient.cs
@@ -0,0 +1,10 @@
+namespace Email_Client_WinForms
+{
+    public partial class EmailClient : Form
+    {
+        public EmailClient()
+        {
+            InitializeComponent();
+        }
+    }
+}
